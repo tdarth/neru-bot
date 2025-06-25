6b0ce0897807ef81a6d3f975c171a7811da15bf1
tdarth <97992436+tdarth@users.noreply.github.com>
2025-06-25 16:06:50 -0700

    Update Link Check to check "EMBED_LINKS" permission
    Why didn't I just do this in the first place?

--- events/linkCheck.js
+++ events/linkCheck.js
@@ -1,9 +1,5 @@
 const { Events, MessageFlags, ContainerBuilder, TextDisplayBuilder } = require('discord.js');
-const { levelRoles, deletedLinksChannelId, staffRoles } = require('../config.json');
-
-const levelRoleIds = Object.entries(levelRoles)
-    .filter(([level]) => level !== "10")
-    .map(([, roleId]) => roleId);
+const { deletedLinksChannelId } = require('../config.json');
 
 const urlRegex = /https?:\/\/[^\s]+|www\.[^\s]+/gi;
 
@@ -12,15 +8,27 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return;
         if (!urlRegex.test(message.content)) return;
-        if (levelRoleIds.some(roleId => message.member.roles.cache.has(roleId))) return;
-        if (message.member.roles.cache.some(role => staffRoles.includes(role.id))) return;
-        if (message.channel.id == '1370819406308835359' || message.channel.id == '1370819438139674634') return;
+        if (
+            message.channel.id === '1370819406308835359' ||
+            message.channel.id === '1370819438139674634'
+        ) return;
+
+        if (message.member.permissionsIn(message.channel).has('EMBED_LINKS')) return;
 
         await message.delete();
-        const sentDeletedMessage = await message.client.channels.cache.get(deletedLinksChannelId).send({ content: message.content });
+        const sentDeletedMessage = await message.client.channels.cache
+            .get(deletedLinksChannelId)
+            .send({ content: message.content });
+
         await sentDeletedMessage.reply({
             flags: MessageFlags.IsComponentsV2,
-            components: [new ContainerBuilder().addTextDisplayComponents(new TextDisplayBuilder().setContent(`Sent by \`${message.author.id}\` in <#${message.channel.id}>. <@${message.author.id}>`))],
+            components: [
+                new ContainerBuilder().addTextDisplayComponents(
+                    new TextDisplayBuilder().setContent(
+                        `Sent by \`${message.author.id}\` in <#${message.channel.id}>. <@${message.author.id}>`
+                    )
+                )
+            ],
             allowedMentions: { parse: [] }
         });
     },
