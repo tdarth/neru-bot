26f207f716c976f2180203cf45a39e7d673be93b
tdarth <97992436+tdarth@users.noreply.github.com>
2025-06-29 14:59:07 -0700

    Add Level 15 Requirement Message

--- events/linkCheck.js
+++ events/linkCheck.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags, PermissionsBitField, ContainerBuilder, TextDisplayBuilder } = require('discord.js');
+const { Events, MessageFlags, PermissionsBitField, ContainerBuilder, TextDisplayBuilder, SeparatorBuilder } = require('discord.js');
 const { deletedLinksChannelId } = require('../config.json');
 
 const urlRegex = /https?:\/\/[^\s]+|www\.[^\s]+/i;
@@ -15,7 +15,9 @@ module.exports = {
 
         if (message.member.permissions.has(PermissionsBitField.Flags.EmbedLinks)) return;
 
+        const level15Message = await message.reply({ flags: MessageFlags.IsComponentsV2, components: [new ContainerBuilder().addTextDisplayComponents(new TextDisplayBuilder().setContent(":x: **Level 15** is required to send gifs/links.")).addSeparatorComponents(new SeparatorBuilder()).addTextDisplayComponents(new TextDisplayBuilder().setContent("-# You can check your progress with **level** in <#1369383513132105874>."))] })
         await message.delete();
+
         const sentDeletedMessage = await message.client.channels.cache
             .get(deletedLinksChannelId)
             .send({ content: message.content, allowedMentions: { parse: [] } });
@@ -31,5 +33,7 @@ module.exports = {
             ],
             allowedMentions: { parse: [] }
         });
+
+        setTimeout(() => { level15Message.delete(); }, 3000);
     },
 };
