a57e68d0d237a5a76ee2be3b0c90eed9edd62354
tdarth <97992436+tdarth@users.noreply.github.com>
2025-07-24 15:44:24 -0700

    Improve ?members

--- events/commands/member-count.js
+++ events/commands/member-count.js
@@ -7,6 +7,11 @@ module.exports = {
     name: 'members',
     trigger: (message) => aliases.some(alias => message.content.toLowerCase() == `${prefix}${alias}`),
     async execute(message) {
-        replyWithText(message, `${emojis.neru} I'm currently watching **${message.guild.memberCount.toString()}** vocaloid fans!`);
+        const response = await fetch('https://discord.com/api/v9/invites/hxVAASyP9e?with_counts=true');
+        if (!response.ok) return replyWithText(message, `:x: **An error occurred.**`);
+
+        const data = await response.json();
+
+        replyWithText(message, `${emojis.neru} I'm currently watching **${data.approximate_presence_count}**/**${data.approximate_member_count}** vocaloid fans!`);
     },
 };
