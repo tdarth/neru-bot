41f542b762cdd438fe917154fe36dd1a0a560b12
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-07 15:40:04 -0700

    Delete events directory

--- events/commands/job.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'job',
-    trigger: (message) => message.content.toLowerCase() == 'job',
-    async execute(message) {
-        await message.reply({ stickers: message.client.guilds.cache.get(message.guild.id).stickers.cache.filter(s => s.id === "1373482200602771486") });
-    },
-};
