779252df1b297329e0da80a6dedb1807422dfc38
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-07 15:40:09 -0700

    Delete utils directory

--- utils/replyWithText.js
+++
@@ -1,9 +0,0 @@
-const { TextDisplayBuilder, ContainerBuilder, MessageFlags } = require("discord.js");
-
-async function replyWithText(message, content) {
-    const textDisplay = new TextDisplayBuilder().setContent(content);
-    const container = new ContainerBuilder().addTextDisplayComponents(textDisplay);
-    await message.reply({ flags: MessageFlags.IsComponentsV2, components: [container] });
-}
-
-module.exports = replyWithText;
--- utils/sendToChannelWithText.js
+++
@@ -1,9 +0,0 @@
-const { TextDisplayBuilder, ContainerBuilder, MessageFlags } = require("discord.js");
-
-async function sendToChannelWithText(client, id, content) {
-    const textDisplay = new TextDisplayBuilder().setContent(content);
-    const container = new ContainerBuilder().addTextDisplayComponents(textDisplay);
-    await client.channels.cache.get(id)?.send({ flags: MessageFlags.IsComponentsV2, components: [container] });
-}
-
-module.exports = sendToChannelWithText;
