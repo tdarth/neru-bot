5f44ec997d5c11d9c12d830bd9e5c2426bfd7bfd
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-08 20:47:51 -0700

    Add catch

--- neru-bot/events/buttonInteractionCreate.js
+++ neru-bot/events/buttonInteractionCreate.js
@@ -6,12 +6,25 @@ module.exports = {
         if (!interaction.isButton()) return;
 
         if (interaction.customId === "markAsResolved_button") {
-            await interaction.message.reply({
-                flags: MessageFlags.IsComponentsV2,
-                components: [new ContainerBuilder().addTextDisplayComponents(new TextDisplayBuilder().setContent("## :white_check_mark: Report Resolved")).addSeparatorComponents(new SeparatorBuilder()).addTextDisplayComponents(new TextDisplayBuilder().setContent(`-# Marked resolved by <@${interaction.user.id}> on <t:${Math.floor(Date.now() / 1000)}:f>.`))]
-            });
-            
-            await interaction.deferUpdate();
+            try {
+                await interaction.deferUpdate();
+
+                await interaction.message.reply({
+                    flags: MessageFlags.IsComponentsV2,
+                    components: [
+                        new ContainerBuilder()
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder().setContent("## :white_check_mark: Report Resolved")
+                            )
+                            .addSeparatorComponents(new SeparatorBuilder())
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder().setContent(`-# Marked resolved by <@${interaction.user.id}> on <t:${Math.floor(Date.now() / 1000)}:f>.`)
+                            )
+                    ]
+                });
+            } catch (err) {
+                console.error(`Error in button interaction: ${err.message}`);
+            }
         }
     },
 };
