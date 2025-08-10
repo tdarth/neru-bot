9ec28ff6a6ef35a23d52d054477c2aec1e1b38ce
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-10 14:05:08 -0700

    Add ?private Command

--- neru-bot/events/buttonInteractionCreate.js
+++ neru-bot/events/buttonInteractionCreate.js
@@ -1,4 +1,5 @@
 const { Events, MessageFlags, TextDisplayBuilder, ContainerBuilder, SeparatorBuilder } = require('discord.js');
+const { staffRoles } = require('../config.json');
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -24,6 +25,32 @@ module.exports = {
                 });
             } catch (err) {
                 console.error(`Error in button interaction: ${err.message}`);
+            }
+        }
+
+        if (interaction.customId === 'private_button') {
+            if (interaction.member.roles.cache.some(role => staffRoles.includes(role.id))) {
+                await interaction.deferUpdate();
+                console.log('yes')
+                const members = await interaction.channel.members.fetch();
+
+                for (const [id] of members) {
+                    interaction.channel.members.remove(id);
+                }
+
+                await interaction.channel.setName(`[LOCKED] ${interaction.channel.name}`);
+                await interaction.channel.setLocked(true);
+            } else {
+                await interaction.reply({
+                    flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
+                    components: [
+                        new ContainerBuilder()
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder()
+                                    .setContent(':x: **Only the moderator can close this thread.**')
+                            )
+                    ]
+                });
             }
         }
     },
---
+++ neru-bot/events/commands/private.js
@@ -0,0 +1,54 @@
+const { ContainerBuilder, MessageFlags, TextDisplayBuilder, SectionBuilder, ButtonBuilder, ButtonStyle, SeparatorBuilder, ThreadAutoArchiveDuration, ChannelType, EmbedBuilder } = require('discord.js');
+const { prefix } = require('../../config.json');
+
+module.exports = {
+    name: 'private',
+    trigger: (message) => message.content.toLowerCase().startsWith(`${prefix}private`),
+    async execute(message) {
+        if (!message.reference) return await message.delete();
+        const referencedMessage = await message.channel.messages.fetch(message.reference.messageId);
+
+        await message.delete();
+
+        const thread = await message.channel.threads.create({
+            name: `private-${referencedMessage.author.username}`,
+            autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+            type: ChannelType.PrivateThread,
+            invitable: false,
+            reason: 'Private thread to discuss moderation.'
+        });
+
+        await thread.members.add(referencedMessage.author.id);
+        await thread.members.add(message.author.id);
+
+        await thread.send({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor('#242429')
+                    .setAuthor({ name: referencedMessage.author.username, iconURL: `https://cdn.discordapp.com/avatars/${referencedMessage.author.id}/${referencedMessage.author.avatar}.png` })
+                    .setDescription(referencedMessage.content || '*Message contained an attachment*')
+            ]
+        });
+
+        await thread.send({
+            flags: MessageFlags.IsComponentsV2,
+            components: [
+                new ContainerBuilder()
+                    .addSectionComponents(
+                        new SectionBuilder()
+                            .addTextDisplayComponents(
+                                new TextDisplayBuilder()
+                                    .setContent(`Private channel opened by <@${message.author.id}>.`)
+                            )
+                            .setButtonAccessory(
+                                new ButtonBuilder()
+                                    .setLabel('Close')
+                                    .setCustomId('private_button')
+                                    .setStyle(ButtonStyle.Danger)
+                            )
+                    )
+            ],
+            allowedMentions: { parse: [] }
+        });
+    },
+};
