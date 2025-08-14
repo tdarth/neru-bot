b4f777dc625c2664b69b38a01a356212b22027da
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-13 22:36:42 -0700

    fix

--- cadmium/commands/level.js
+++ cadmium/commands/level.js
@@ -9,7 +9,7 @@ module.exports = {
 		.setDescription("View you or a user's level")
 		.addUserOption(option => option.setName('member').setDescription('The member to view').setRequired(false)),
 	async execute(interaction) {
-		const targetId = interaction?.options?.getString('member')?.id || interaction.user.id;
+		const targetId = interaction?.options?.getUser('member')?.id || interaction.user.id;
 
 		const xp = await getXp(interaction.guild.id, targetId);
 		if (!xp) return await interaction.reply(new ContainerMessage(messages.info.USER_HAS_NO_XP.replace('USER', `<@${targetId}>`)).build());
