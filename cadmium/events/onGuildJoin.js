81491d4ecf696bc823d22853ab2d5e97701c3a83
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-13 23:20:40 -0700

    fix

--- cadmium/events/onGuildJoin.js
+++ cadmium/events/onGuildJoin.js
@@ -3,8 +3,10 @@ const { servers } = require('../allowed_servers.json');
 
 module.exports = {
     name: Events.GuildCreate,
-    once: true,
     async execute(guild) {
-        if (!servers.includes(guild.id)) await guild.leave();
+        if (!servers.includes(guild.id)) {
+            console.log(`[CADMIUM] Left ${guild.name} (${guild.id}), not on whitelist.`);
+            await guild.leave();
+        }
     },
 };
