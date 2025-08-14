5aa4203142563580b22ee7c45827971d736e8357
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 15:31:35 -0700

    fix

--- cadmium/events/leveling/messageSent.js
+++ cadmium/events/leveling/messageSent.js
@@ -7,11 +7,8 @@ const ContainerMessage = require('../../utils/classes/ContainerMessage');
 module.exports = {
     name: Events.MessageCreate,
     async execute(message) {
-        const serverConfig = getServerConfig(message.guild.id);
-        const userData = getUserData(message.guild.id, message.author.id);
-
-        console.log('message detected');
-        console.log(serverConfig.xp_enabled);
+        const serverConfig = await getServerConfig(message.guild.id);
+        const userData = await getUserData(message.guild.id, message.author.id);
 
         if (serverConfig.xp_enabled) {
             console.log(message.createdTimestamp);
