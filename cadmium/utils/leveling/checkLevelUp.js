14a0f63ce2332600b0f9ff0468939840d13bb35d
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 16:43:09 -0700

    fix

--- cadmium/utils/leveling/checkLevelUp.js
+++ cadmium/utils/leveling/checkLevelUp.js
@@ -9,7 +9,7 @@ async function checkLevelUp(serverId, userId) {
     if (userData.xp >= userData.next_level_xp) {
         await updateUserData(serverId, userId, 'xp', 0);
         await updateUserData(serverId, userId, 'level', userData.level + 1);
-        await updateUserData(serverId, userId, 'next_level_xp', serverConfig.xp_levelup_mode == 'fixed' ? serverConfig.xp_levelup_amount : Math.floor(userData.next_level_xp + serverConfig.xp_levelup_amount * serverConfig.xp_levelup_multiplier));
+        await updateUserData(serverId, userId, 'next_level_xp', serverConfig.xp_levelup_mode == 'fixed' ? serverConfig.xp_levelup_amount : Math.floor((userData.next_level_xp + serverConfig.xp_levelup_amount) * serverConfig.xp_levelup_multiplier));
     }
 }
 
