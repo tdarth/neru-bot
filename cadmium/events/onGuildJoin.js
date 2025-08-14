4fe7f4d22ddd53282440b2c2815523ec412e18a5
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-13 23:28:33 -0700

    change

--- cadmium/events/onGuildJoin.js
+++ cadmium/events/onGuildJoin.js
@@ -1,12 +1,21 @@
 const { Events } = require('discord.js');
 const { servers } = require('../allowed_servers.json');
+const ContainerMessage = require('../utils/classes/ContainerMessage');
 
 module.exports = {
     name: Events.GuildCreate,
     async execute(guild) {
         if (!servers.includes(guild.id)) {
-            console.log(`[CADMIUM] Left ${guild.name} (${guild.id}), not on whitelist.`);
+            const owner = await guild.client.users.fetch(guild.ownerId);
+
+            try {
+                await owner.send(new ContainerMessage(`:wave: Hi **${owner.username}**!\n*Cadmium* is currently whitelist-only, meaning only approved servers can add the bot. Please message <@990500436047982602> (\`tdarth_\`) for more information.`).build())
+            } catch (err) {
+                console.log('[CADMIUM] Owner DM failed.');
+            }
+
             await guild.leave();
+            console.log(`[CADMIUM] Left guild ${guild.name} (${guild.id}) (${guild.memberCount} members) (${guild.ownerId} Owner ID), not on whitelist.`);
         }
     },
 };
