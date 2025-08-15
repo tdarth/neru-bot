77ef53ebaf3d7e2b10839ea158095a97ae854968
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 21:24:29 -0700

    fix

--- cadmium/commands/xp/add.js
+++ cadmium/commands/xp/add.js
@@ -1,5 +1,6 @@
 const { SlashCommandSubcommandBuilder } = require('discord.js');
 const { forceAddXp } = require('../../utils/leveling/forceAddXp');
+const { getLevel } = require('../../utils/leveling/getLevel');
 const { formatNumber } = require('../../utils/formatNumber');
 const { messages } = require('../../messages.json');
 const ContainerMessage = require('../../utils/classes/ContainerMessage');
@@ -15,12 +16,13 @@ module.exports = {
         if (isNaN(amount)) return interaction.reply(new ContainerMessage(messages.errors.MUST_BE_NUMBER.replaceAll('{argument}', 'amount')).isEphemeral().build());
 
         const user = interaction.options.getUser('member');
+        const oldData = await getLevel(interaction.guild.id, interaction.user.id);
         const result = await forceAddXp(interaction.guild.id, user.id, amount);
 
         await interaction.reply(new ContainerMessage(messages.success.ADDED_XP
             .replaceAll('{amount}', amount.toLocaleString())
             .replaceAll('{user}', `<@${user.id}>`)
-            .replaceAll('{old_amount}', `${result.oldXp.toLocaleString()} (${result.oldTotal.toLocaleString()} total)`)
+            .replaceAll('{old_amount}', `${oldData.xp.toLocaleString()} (${oldData.totalXp.toLocaleString()} total)`)
             .replaceAll('{new_amount}', `${result.newXp.toLocaleString()} (${result.newTotal.toLocaleString()} total)`)
         ).build());
     }
--- cadmium/commands/xp/remove.js
+++ cadmium/commands/xp/remove.js
@@ -1,5 +1,6 @@
 const { SlashCommandSubcommandBuilder } = require('discord.js');
 const { forceRemoveXp } = require('../../utils/leveling/forceRemoveXp');
+const { getLevel } = require('../../utils/leveling/getLevel');
 const { formatNumber } = require('../../utils/formatNumber');
 const { messages } = require('../../messages.json');
 const ContainerMessage = require('../../utils/classes/ContainerMessage');
@@ -15,12 +16,13 @@ module.exports = {
         if (isNaN(amount)) return interaction.reply(new ContainerMessage(messages.errors.MUST_BE_NUMBER.replaceAll('{argument}', 'amount')).isEphemeral().build());
 
         const user = interaction.options.getUser('member');
-        const result = await forceRemoveXp(interaction.guild.id, user.id, amount);
+        const oldData = await getLevel(interaction.guild.id, interaction.user.id);
+        const result = await forceAddXp(interaction.guild.id, user.id, amount);
         
         await interaction.reply(new ContainerMessage(messages.success.REMOVED_XP
             .replaceAll('{amount}', amount.toLocaleString())
             .replaceAll('{user}', `<@${user.id}>`)
-            .replaceAll('{old_amount}', `${result.oldXp.toLocaleString()} (${result.oldTotal.toLocaleString()} total)`)
+            .replaceAll('{old_amount}', `${oldData.xp.toLocaleString()} (${oldData.totalXp.toLocaleString()} total)`)
             .replaceAll('{new_amount}', `${result.newXp.toLocaleString()} (${result.newTotal.toLocaleString()} total)`)
         ).build());
     }
