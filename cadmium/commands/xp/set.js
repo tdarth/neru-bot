7ce0f70ed770b0457879ada6f31db20d11787ff9
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 21:51:34 -0700

    add

---
+++ cadmium/commands/xp/set.js
@@ -0,0 +1,30 @@
+const { SlashCommandSubcommandBuilder } = require('discord.js');
+const { forceSetXp } = require('../../utils/leveling/forceSetXp');
+const { getLevel } = require('../../utils/leveling/getLevel');
+const { formatNumber } = require('../../utils/formatNumber');
+const { messages } = require('../../messages.json');
+const ContainerMessage = require('../../utils/classes/ContainerMessage');
+
+module.exports = {
+    data: new SlashCommandSubcommandBuilder()
+        .setName('set')
+        .setDescription("Sets the XP of a member")
+        .addUserOption(option => option.setName('member').setDescription('The member to modify').setRequired(true))
+        .addStringOption(option => option.setName('amount').setDescription('The amount').setRequired(true)),
+	async execute(interaction) {
+        const amount = formatNumber(parseInt(interaction.options.getString('amount'), 10));
+        if (isNaN(amount)) return interaction.reply(new ContainerMessage(messages.errors.MUST_BE_NUMBER.replaceAll('{argument}', 'amount')).isEphemeral().build());
+
+        const user = interaction.options.getUser('member');
+        const oldData = await getLevel(interaction.guild.id, interaction.user.id);
+        await forceSetXp(interaction.guild.id, user.id, amount);
+        const newData = await getLevel(interaction.guild.id, interaction.user.id);
+        
+        await interaction.reply(new ContainerMessage(messages.success.SET_XP
+            .replaceAll('{amount}', amount.toLocaleString())
+            .replaceAll('{user}', `<@${user.id}>`)
+            .replaceAll('{old_amount}', `${oldData.xp.toLocaleString()} (${oldData.totalXp.toLocaleString()} total)`)
+            .replaceAll('{new_amount}', `${newData.xp.toLocaleString()} (${newData.totalXp.toLocaleString()} total)`)
+        ).build());
+    }
+};
---
+++ cadmium/utils/leveling/forceSetXp.js
@@ -0,0 +1,9 @@
+const updateXp = require('./updateXp');
+const { checkLevelUp } = require('./checkLevelUp');
+
+async function forceSetXp(serverId, userId, amount, channel = null) {
+    await updateXp(serverId, userId, amount);
+    return await checkLevelUp(serverId, userId, channel);
+}
+
+module.exports = { forceSetXp };
