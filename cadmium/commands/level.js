be56208600114c0a39e84e15add72ada0cbf15af
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 17:28:53 -0700

    add

--- cadmium/commands/level.js
+++ cadmium/commands/level.js
@@ -9,13 +9,15 @@ module.exports = {
 		.setDescription("View you or a user's level")
 		.addUserOption(option => option.setName('member').setDescription('The member to view').setRequired(false)),
 	async execute(interaction) {
-		const targetId = interaction?.options?.getUser('member')?.id || interaction.user.id;
+		const targetUser = interaction?.options?.getUser('member') || interaction.user.id;
 
-		const level = await getLevel(interaction.guild.id, targetId);
+		if (targetUser.bot) return await interaction.reply(new ContainerMessage().build());
+
+		const level = await getLevel(interaction.guild.id, targetUser.id);
 		if ((level.xp == 0 || level.xp == null) && level.level == 0) return await interaction.reply(new ContainerMessage(messages.info.USER_HAS_NO_XP.replace('{user}', `<@${targetId}>`)).build());
 
 		await interaction.reply(new ContainerMessage(messages.info.USER_HAS_XP
-			.replaceAll('{user}', `<@${targetId}>`)
+			.replaceAll('{user}', `<@${targetUser.id}>`)
 			.replaceAll('{level}', level.level.toLocaleString())
 			.replaceAll('{xp}', level.xp.toLocaleString())
 			.replaceAll('{nextLevelXp}', level.nextLevelXp.toLocaleString())
