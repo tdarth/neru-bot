5c4deabb7415c05dfdb6c1c705a763a40417445a
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-14 21:18:06 -0700

    fix

--- cadmium/utils/leveling/checkLevelUp.js
+++ cadmium/utils/leveling/checkLevelUp.js
@@ -62,7 +62,7 @@ async function checkLevelUp(serverId, userId, channel = null) {
 
         if (serverConfig.level_up_message_card_enabled == 1) {
             let messageCard = await generateMessageCard({
-                title: serverConfig.level_up_message_card_displayname_enabled ? user.displayName || user.username : user.username,
+                title: serverConfig.level_up_message_card_displayname_enabled == 1 ? user.displayName || user.username : user.username,
                 description: levelUpMessageCard,
                 avatar: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`,
                 bg_color: '#202024',
