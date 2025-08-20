b68f7d8064b7d63925d7b5824114ad31a44b623c
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-19 22:19:44 -0700

    fix

--- cadmium/commands/rewards/add.js
+++ cadmium/commands/rewards/add.js
@@ -14,9 +14,10 @@ module.exports = {
 		if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return interaction.reply(new ContainerMessage(messages.errors.MISSING_PERMISSION.replaceAll('{permission}', 'administrator')).isEphemeral().build());
 		const role = interaction.options.getRole('role');
         const level = formatNumber(parseInt(interaction.options.getString('level'), 10));
+        const botMember = interaction.guild.members.me;
 
         if (isNaN(level)) return interaction.reply(new ContainerMessage(messages.errors.MUST_BE_NUMBER.replaceAll('{argument}', 'level')).isEphemeral().build());
-        if (interaction.guild.members.me.roles.highest.position <= role.position) return interaction.reply(new ContainerMessage(messages.errors.BOT_ROLE_TOO_LOW.replaceAll('{botRole}', `<@&${interaction.guild.members.me.roles.cache.find(role => role.tags?.botId === botMember.id)}>`).replaceAll('{role}', `<@&${role.id}>`)).isEphemeral().build());
+        if (botMember.roles.highest.position <= role.position) return interaction.reply(new ContainerMessage(messages.errors.BOT_ROLE_TOO_LOW.replaceAll('{botRole}', `<@&${botMember.roles.cache.find(role => role.tags?.botId === botMember.id)}>`).replaceAll('{role}', `<@&${role.id}>`)).isEphemeral().build());
 
 		await addLevelRole(interaction.guild.id, role.id, level);
 		await interaction.reply(new ContainerMessage(messages.success.ROLE_REWARD_ADDED.replaceAll('{role}', role.id).replaceAll('{level}', level)).build());
