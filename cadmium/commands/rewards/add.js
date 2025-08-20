6b8bc8adf875f6bd2135138b2830d0ae11bf0d64
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-19 22:14:50 -0700

    fix

--- cadmium/commands/rewards/add.js
+++ cadmium/commands/rewards/add.js
@@ -16,7 +16,7 @@ module.exports = {
         const level = formatNumber(parseInt(interaction.options.getString('level'), 10));
 
         if (isNaN(level)) return interaction.reply(new ContainerMessage(messages.errors.MUST_BE_NUMBER.replaceAll('{argument}', 'level')).isEphemeral().build());
-        if (interaction.guild.members.me.roles.highest.position <= role.position) return interaction.reply(new ContainerMessage(messages.errors.BOT_ROLE_TOO_LOW.replaceAll('{botRole}', `<@&${interaction.client.id}>`).replaceAll('{role}', `<@&${role.id}>`)).isEphemeral().build());
+        if (interaction.guild.members.me.roles.highest.position <= role.position) return interaction.reply(new ContainerMessage(messages.errors.BOT_ROLE_TOO_LOW.replaceAll('{botRole}', `<@&${interaction.client.user.id}>`).replaceAll('{role}', `<@&${role.id}>`)).isEphemeral().build());
 
 		await addLevelRole(interaction.guild.id, role.id, level);
 		await interaction.reply(new ContainerMessage(messages.success.ROLE_REWARD_ADDED.replaceAll('{role}', role.id).replaceAll('{level}', level)).build());
