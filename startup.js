1be3360c9395e0093a5711f5a08fe30b1f88f8e4
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-22 21:41:49 -0700

    startup

--- startup.js
+++ startup.js
@@ -13,11 +13,15 @@ run("npm", ["install"], "./neru-bot")
       .on("close", () => {
         run("npm", ["install"], "./cadmium")
           .on("close", () => {
-            run("node", ["index.js"], "./neru-bot");
-            run("node", ["index.js"], "./teto-bot");
-            run("node", ["index.js"], "./cadmium");
+            run("npm", ["install"], "./miku-bot")
+              .on("close", () => {
+                run("node", ["index.js"], "./neru-bot");
+                run("node", ["index.js"], "./teto-bot");
+                run("node", ["index.js"], "./cadmium");
+                run("node", ["index.js"], "./miku-bot");
+              });
           });
       });
   });
 
-console.log('Began startup script.');
+console.log("Began startup script.");
