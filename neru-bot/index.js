84a9101b68b842f383cd81e89583cddf0efef238
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-23 15:34:03 -0700

    debug

--- neru-bot/index.js
+++ neru-bot/index.js
@@ -149,6 +149,23 @@ app.get('/config', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.originalUrl}`);
+    console.log(`Headers:`, req.headers);
+    if (req.body && Object.keys(req.body).length > 0) {
+        console.log(`Body:`, req.body);
+    }
+
+    const originalSend = res.send;
+    res.send = function (body) {
+        console.log(`[RESPONSE] Status: ${res.statusCode}`);
+        console.log(`Body:`, body);
+        originalSend.call(this, body);
+    };
+
+    next();
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
