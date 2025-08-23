b3844a2698549e7a26044cc3811e1c2f3570993c
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-22 21:23:45 -0700

    no more fun

--- neru-bot/events/messageCreate.js
+++ neru-bot/events/messageCreate.js
@@ -11,7 +11,7 @@ module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
     if (message.author.bot) return;
-    await message.reply({ stickers: message.client.guilds.cache.get(message.guild.id).stickers.cache.filter(s => s.id === "1389298310875058216") });
+    // await message.reply({ stickers: message.client.guilds.cache.get(message.guild.id).stickers.cache.filter(s => s.id === "1389298310875058216") });
 
     if (message.guild) {
       for (const trigger of message.client.triggers) {
