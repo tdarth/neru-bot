aec16055ea5f188b56346716edb17f77a00b4a6a
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-24 04:23:33 -0700

    fix

--- cadmium/utils/leveling/addXp.js
+++ cadmium/utils/leveling/addXp.js
@@ -8,7 +8,7 @@ async function addXp(serverId, userId, type, channel = null) {
 
     if (type == 'msg') {
         await updateUserData(serverId, userId, 'last_message', toMySQLDate(Date.now()));
-        await forceAddXp(serverId, userId, serverConfig.xp_mode == 'fixed' ? serverConfig.xp_static : Math.floor(Math.random() * (serverConfig.xp_random_max - serverConfig.xp_random_min + 1)) + serverConfig.xp_random_min, channel);
+        await forceAddXp(serverId, userId, serverConfig.xp_mode == 'static' ? serverConfig.xp_static : Math.floor(Math.random() * (serverConfig.xp_random_max - serverConfig.xp_random_min + 1)) + serverConfig.xp_random_min, channel);
     }
 }
 
