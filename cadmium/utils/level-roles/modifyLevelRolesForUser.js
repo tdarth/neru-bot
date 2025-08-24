2dd25c4d795f5ff9cf21b1ac47fa18a701861fc0
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-24 01:24:22 -0700

    fix

--- cadmium/utils/level-roles/modifyLevelRolesForUser.js
+++ cadmium/utils/level-roles/modifyLevelRolesForUser.js
@@ -1,38 +1,40 @@
 async function modifyLevelRolesForUser(member, roles, stackEnabled) {
     const memberRoles = member.roles.cache.map(role => role.id);
-    const botHighestRole = member.guild.me.roles.highest;
-
-    const canManageRole = (roleId) => {
-        const role = member.guild.roles.cache.get(roleId);
-        return role && botHighestRole.position > role.position;
-    };
 
     if (stackEnabled) {
-        const rolesToAdd = roles.filter(roleId => !memberRoles.includes(roleId) && canManageRole(roleId));
-        const rolesToRemove = memberRoles.filter(roleId => !roles.includes(roleId) && roleId !== member.guild.id && canManageRole(roleId));
+        const rolesToAdd = roles.filter(roleId => !memberRoles.includes(roleId));
+        const rolesToRemove = memberRoles.filter(roleId => !roles.includes(roleId) && roleId !== member.guild.id);
 
         if (rolesToAdd.length) {
-            try { await member.roles.add(rolesToAdd); } catch (err) { }
+            try {
+                await member.roles.add(rolesToAdd);
+            } catch {}
         }
         if (rolesToRemove.length) {
-            try { await member.roles.remove(rolesToRemove); } catch (err) { }
+            try {
+                await member.roles.remove(rolesToRemove);
+            } catch {}
         }
     } else {
         if (roles.length === 0) {
-            const rolesToRemove = memberRoles.filter(roleId => roleId !== member.guild.id && canManageRole(roleId));
+            const rolesToRemove = memberRoles.filter(roleId => roleId !== member.guild.id);
             if (rolesToRemove.length) {
-                try { await member.roles.remove(rolesToRemove); } catch (err) { }
+                try {
+                    await member.roles.remove(rolesToRemove);
+                } catch {}
             }
         } else {
             const highestRoleId = roles[roles.length - 1];
-            const rolesToRemove = memberRoles.filter(roleId => roleId !== highestRoleId && roleId !== member.guild.id && canManageRole(roleId));
-
+            const rolesToRemove = memberRoles.filter(roleId => roleId !== highestRoleId && roleId !== member.guild.id);
             if (rolesToRemove.length) {
-                try { await member.roles.remove(rolesToRemove); } catch (err) { }
+                try {
+                    await member.roles.remove(rolesToRemove);
+                } catch {}
             }
-
-            if (!memberRoles.includes(highestRoleId) && canManageRole(highestRoleId)) {
-                try { await member.roles.add(highestRoleId); } catch (err) { }
+            if (!memberRoles.includes(highestRoleId)) {
+                try {
+                    await member.roles.add(highestRoleId);
+                } catch {}
             }
         }
     }
