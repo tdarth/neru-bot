c241c39991a03b197ffe709ae228041aad440439
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-30 14:31:19 -0700

    debug

--- tagroles/commands/info.js
+++ tagroles/commands/info.js
@@ -6,5 +6,8 @@ module.exports = {
         .setDescription("Shows your profile"),
     async execute(interaction) {
         await interaction.reply(`hi\n${interaction.user.primaryGuild.tag}\n${interaction.user.primaryGuild.badge}\n${interaction.user.primaryGuild.identityGuildId}`);
+
+        const cached = interaction.guild.members.cache.size;
+        await interaction.reply(`cached: ${cached}`);
     }
 };
