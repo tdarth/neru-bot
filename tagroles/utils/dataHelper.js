5bb6136a9bc2e8fd26a3daefffa7a9d404b27edb
tdarth <97992436+tdarth@users.noreply.github.com>
2025-08-30 14:07:07 -0700

    fix

--- tagroles/utils/dataHelper.js
+++ tagroles/utils/dataHelper.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
