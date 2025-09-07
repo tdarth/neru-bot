5f3b3215e8735b3ed1d15dd78005a91228bb557d
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-07 03:41:05 -0700

    add character info

--- neru-bot/events/commands/umamusume.js
+++ neru-bot/events/commands/umamusume.js
@@ -14,6 +14,18 @@ module.exports = {
         const randomChar = characters[Math.floor(Math.random() * characters.length)];
         const charId = randomChar.web_id;
 
+        const characterInfoRes = await fetch(`https://umapyoi.net/api/v1/character/${charId}`);
+        if (!characterInfoRes.ok) return replyWithText(message, ':x: **An error occurred.**');
+        const characterInfo = await characterInfoRes.json();
+
+        let info = "";
+
+        if (characterInfo?.name_en && characterInfo?.name_jp) info += `**${characterInfo.name_en} \`${characterInfo.name_jp}\`**`;
+        else if (characterInfo?.name_en) info += `**${characterInfo.name_en}**`;
+        else if (characterInfo?.name_jp) info += `**${characterInfo.name_jp}**`;
+
+        if (characterInfo?.profile) info += `\n\`\`\`${characterInfo.profile}\`\`\``;
+
         const imagesRes = await fetch(`https://umapyoi.net/api/v1/character/images/${charId}`);
         if (!imagesRes.ok) return replyWithText(message, ':x: **An error occurred.**');
         const images = await imagesRes.json();
@@ -22,6 +34,6 @@ module.exports = {
         const allImages = categories.flatMap(cat => cat.images);
         const randomImage = allImages[Math.floor(Math.random() * allImages.length)];
 
-        await message.reply(`[Loading..](${randomImage.image})`);
+        await message.reply(`${info}\n[Image](${randomImage.image})`);
     },
 };
