58a584ce4c621bf43ae1bb8998218ad9eb128ebe
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-07 17:13:13 -0700

    stare

--- teto-bot/events/commands/baguette.js
+++ teto-bot/events/commands/baguette.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'baguette',
     trigger: (message) => message.content.toLowerCase() == 'baguette' || message.content.toLowerCase().match(/^(?=.*\bjob\b)(?=.*\bbaguette\b)(?=.*\bleek\b)\b(job|baguette|leek)\b(?:\s+(job|baguette|leek)\b){2}$/),
     async execute(message) {
-        await message.reply(':french_bread:');
+        await message.reply('https://i.imgur.com/S1PQ7tt.png');
     },
 };
