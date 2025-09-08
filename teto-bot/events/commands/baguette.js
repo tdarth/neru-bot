8907140a7729b5c921145d1517826667c3e1b32f
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-07 17:37:45 -0700

    reenable

--- teto-bot/events/commands/baguette.js
+++ teto-bot/events/commands/baguette.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'baguette',
     trigger: (message) => message.content.toLowerCase() == 'baguette' || message.content.toLowerCase().match(/^(?=.*\bjob\b)(?=.*\bbaguette\b)(?=.*\bleek\b)\b(job|baguette|leek)\b(?:\s+(job|baguette|leek)\b){2}$/),
     async execute(message) {
-        return;
         await message.reply(':french_bread:');
     },
 };
