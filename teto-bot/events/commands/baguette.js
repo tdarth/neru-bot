ca4fd1302f08fcb850a724e44b58514e02f4752a
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-07 17:30:27 -0700

    disable

--- teto-bot/events/commands/baguette.js
+++ teto-bot/events/commands/baguette.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'baguette',
     trigger: (message) => message.content.toLowerCase() == 'baguette' || message.content.toLowerCase().match(/^(?=.*\bjob\b)(?=.*\bbaguette\b)(?=.*\bleek\b)\b(job|baguette|leek)\b(?:\s+(job|baguette|leek)\b){2}$/),
     async execute(message) {
+        return;
         await message.reply(':french_bread:');
     },
 };
