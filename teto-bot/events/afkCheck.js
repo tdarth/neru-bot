3287f4a96dc504f030a0915c35075974469b3ba1
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-10 22:40:46 -0700

    test

--- teto-bot/events/afkCheck.js
+++ teto-bot/events/afkCheck.js
@@ -11,7 +11,7 @@ module.exports = {
         const id = message.author.id;
 
         if (id in afkUsers) {
-            await message.react('<:teto_wave:1369235578427670570>');
+            await message.react('👋');
 
             if (message.member && message.member.manageable) {
                 await message.member.setNickname(afkUsers[id].username, `User returned.`).catch(error => {
