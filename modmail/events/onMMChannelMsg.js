13ececfc6248944a8928e645d398006468b39d37
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-19 22:47:09 -0700

    revert

--- modmail/events/onMMChannelMsg.js
+++ modmail/events/onMMChannelMsg.js
@@ -38,8 +38,6 @@ module.exports = {
 
             const files = message.attachments.size > 0 ? Array.from(message.attachments.values()) : undefined;
 
-            if (message.stickers.size > 0 || message.attachments.size > 0) embed.setFooter(`Message contains ${message.stickers.size} + ${message.attachments.size} attachments.`);
-
             try {
                 await user.send({
                     embeds: [embed],
