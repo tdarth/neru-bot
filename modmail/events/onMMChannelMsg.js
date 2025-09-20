dfb4b566229f0ad95f4eb38f09eaea74e134bd3d
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-19 22:46:04 -0700

    fix

--- modmail/events/onMMChannelMsg.js
+++ modmail/events/onMMChannelMsg.js
@@ -38,7 +38,7 @@ module.exports = {
 
             const files = message.attachments.size > 0 ? Array.from(message.attachments.values()) : undefined;
 
-            if (message.stickers.size > 0 && message.attachments.size > 0) embed.setFooter(`Message contains ${message.stickers.size} + ${message.attachments.size} attachments.`);
+            if (message.stickers.size > 0 || message.attachments.size > 0) embed.setFooter(`Message contains ${message.stickers.size} + ${message.attachments.size} attachments.`);
 
             try {
                 await user.send({
