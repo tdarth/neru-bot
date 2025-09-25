66fa345de25ccf4bba29813a78c51f4e62531e8d
tdarth <97992436+tdarth@users.noreply.github.com>
2025-09-24 22:51:24 -0700

    changes

---
+++ neru-bot/events/commands/close.js
@@ -0,0 +1,22 @@
+const { staffRoles, prefix } = require('../../config.json');
+const replyWithText = require('../../utils/replyWithText');
+
+module.exports = {
+    name: 'close',
+    trigger: (message) => message.content.startsWith(`${prefix}close`),
+    async execute(message) {
+        if (!message.channel.name.includes("private-")) return replyWithText(":x: **This command can only be used in private threads.**");
+        if (message.member.roles.cache.some(role => staffRoles.includes(role.id))) {
+            const members = await message.channel.members.fetch();
+
+            for (const [id] of members) {
+                message.channel.members.remove(id);
+            }
+
+            await message.channel.setName(`[LOCKED] ${interaction.channel.name}`);
+            await message.channel.setLocked(true);
+        } else {
+            replyWithText(message, ':x: **Only moderators can close private threads.**')
+        }
+    },
+};
