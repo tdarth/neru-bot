29a67990527b900cb19ddd324cc1f4c21a12d881
tdarth <97992436+tdarth@users.noreply.github.com>
2025-10-09 23:38:37 -0700

    disable unused bot

--- startup.js
+++ startup.js
@@ -11,8 +11,8 @@ run("npm", ["install"], "./neru-bot")
   .on("close", () => {
     run("npm", ["install"], "./teto-bot")
       .on("close", () => {
-        run("npm", ["install"], "./cadmium")
-          .on("close", () => {
+        // run("npm", ["install"], "./cadmium")
+          // .on("close", () => {
             run("npm", ["install"], "./miku-bot")
               .on("close", () => {
                 run("npm", ["install"], "./tagroles")
@@ -21,14 +21,14 @@ run("npm", ["install"], "./neru-bot")
                       .on("close", () => {
                         run("node", ["index.js"], "./neru-bot");
                         run("node", ["index.js"], "./teto-bot");
-                        run("node", ["index.js"], "./cadmium");
+                        // run("node", ["index.js"], "./cadmium");
                         run("node", ["index.js"], "./miku-bot");
                         run("node", ["index.js"], "./tagroles");
                         run("node", ["index.js"], "./modmail");
                       });
                   });
               });
-          });
+          // });
       });
   });
 
