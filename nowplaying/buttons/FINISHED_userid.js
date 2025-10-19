fc23b4c8c2945d81eefe20b06ccb87f9887cf1cb
tdarth <97992436+tdarth@users.noreply.github.com>
2025-10-18 19:52:47 -0700

    fix error

--- nowplaying/buttons/FINISHED_userid.js
+++ nowplaying/buttons/FINISHED_userid.js
@@ -1,3 +1,4 @@
+const { ActionRowBuilder, ButtonBuilder } = require('discord.js')
 const ContainerMessage = require('../utils/classes/ContainerMessage');
 const { emojis } = require('../config.json');
 const { getUserData } = require('../utils/lastfm/getUserData');
