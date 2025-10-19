e68af79d5ae22a9ed3ef1fdfd0e7af3fdc82d964
tdarth <97992436+tdarth@users.noreply.github.com>
2025-10-18 20:13:55 -0700

    debug

--- nowplaying/utils/lastfm/getSessionToken.js
+++ nowplaying/utils/lastfm/getSessionToken.js
@@ -4,8 +4,8 @@ const cache = {};
 
 async function getSessionToken(id) {
     if (cache[id]) {
+        console.log(cache[id])
         if (await getUserData(cache[id])) {
-            console.log(cache[id])
             return cache[id];
         }
     }
