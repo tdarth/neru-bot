79111d0fae05207263eff44f1a37c01a9ad872fd
tdarth <97992436+tdarth@users.noreply.github.com>
2025-10-25 13:17:55 -0700

    exclude counting channel

--- teto-bot/events/commands/baguette.js
+++ teto-bot/events/commands/baguette.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'baguette',
     trigger: (message) => message.content.toLowerCase() == 'baguette' || message.content.toLowerCase().match(/^(?=.*\bjob\b)(?=.*\bbaguette\b)(?=.*\bleek\b)\b(job|baguette|leek)\b(?:\s+(job|baguette|leek)\b){2}$/),
     async execute(message) {
+        if (message.channel.id == "1399976818605297825") return; // #counting
         await message.reply(':french_bread:');
     },
 };
