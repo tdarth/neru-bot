c129c71b6cea0cb1992b05afc6bf60919c3677f2
tdarth <97992436+tdarth@users.noreply.github.com>
2025-11-16 23:44:14 -0700

    remove command

--- teto-bot/events/commands/cool.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'cool',
-    trigger: (message) => message.content.toLowerCase() == 'tdarth',
-    async execute(message) {
-        await message.reply('is so cool');
-    },
-};
