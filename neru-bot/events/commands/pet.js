8870b0355bef9f8e77432f062ba98435070672b3
tdarth <97992436+tdarth@users.noreply.github.com>
2025-11-19 17:36:09 -0700

    add pet command to neru bot

---
+++ neru-bot/events/commands/pet.js
@@ -0,0 +1,21 @@
+const { AttachmentBuilder } = require('discord.js');
+const { prefix } = require('../../config.json');
+const replyWithText = require('../../utils/replyWithText')
+
+module.exports = {
+    name: 'pet',
+    trigger: (message) => message.content.startsWith(`${prefix}pet`),
+    async execute(message) {
+        await message.channel.sendTyping();
+
+        const user = message.mentions.users.first() || message.author;
+        if (!user) return await replyWithText(message, ':x: **An error occurred. Yikes!**');
+
+        const res = await fetch(`https://api.some-random-api.com/premium/petpet?avatar=${user.displayAvatarURL({extension: 'png'})}`);
+        if (!res.ok) return await replyWithText(message, ':x: **An error occurred. Yikes!**');
+        
+        await message.reply({
+            files: [new AttachmentBuilder(Buffer.from(await res.arrayBuffer()), {name: `petpet-${user.username || user.id || 'unknown'}.gif`})]
+        })
+    },
+};
