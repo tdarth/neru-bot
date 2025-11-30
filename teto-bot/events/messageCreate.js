01102b3d86b5a097927bbc744eada67995f12f06
tdarth <97992436+tdarth@users.noreply.github.com>
2025-11-30 13:51:45 -0700

    revert

--- teto-bot/events/messageCreate.js
+++ teto-bot/events/messageCreate.js
@@ -4,9 +4,6 @@ module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
     if (message.author.bot) return;
-    if (message.guild.id == '1369247242724179998') return;
-
-    await message.reply(`<:teto_middlefinger:1440902677079920731>`);
 
     if (message.guild) {
       for (const trigger of message.client.triggers) {
