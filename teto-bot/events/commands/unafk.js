109c060d3058c073328ea9aeb4f732985cc3b0bc
tdarth <97992436+tdarth@users.noreply.github.com>
2025-12-05 21:46:04 -0700

    fix

--- teto-bot/events/commands/unafk.js
+++ teto-bot/events/commands/unafk.js
@@ -3,11 +3,15 @@ const replyWithText = require('../../utils/replyWithText');
 const { prefix, staffRoles } = require('../../config.json');
 const { afkUsers, deleteAfkUser } = require('../../utils/afkHelper');
 
+const neruStaffRoles = ["1370790301572272370", "1369249545959247932", "1369834138634293251", "1369834948386623518"];
+
 module.exports = {
     name: 'unafk',
     trigger: (message) => message.content.startsWith(`${prefix}unafk`),
     async execute(message) {
-        if (!message.member.roles.cache.some(role => staffRoles.includes(role.id))) return await message.reply('<:teto_think:1393360916090982510>');
+        if (message.guild.id == "1369181065385869352" && !message.member.roles.cache.some(role => staffRoles.includes(role.id))) return await message.reply('<:teto_think:1393360916090982510>');
+        if (message.guild.id == "1369247242724179998" && !message.member.roles.cache.some(role => neruStaffRoles.includes(role.id))) return await message.reply('<:teto_think:1393360916090982510>');
+        
         const id = message.content.replace(`${prefix}unafk`, '').replace('<', '').replace('>', '').replace('@', '').trim();
         if (!id) return replyWithText(message, ':x: **Usage: ?unafk <user/userid>**');
         const member = await message.guild.members.fetch(id);
