3663d8d69bd3bf10c215a4412d145f9ee90a9846
tdarth <97992436+tdarth@users.noreply.github.com>
2025-12-06 13:20:11 -0700

    revert

--- vocavote/utils/cleanForMatch.js
+++
@@ -1,5 +0,0 @@
-function cleanForMatch(str) {
-  return str.toLowerCase().replace(/[^a-z0-9]/g, '');
-}
-
-module.exports = { cleanForMatch }
--- vocavote/utils/cleanVoteData.js
+++ vocavote/utils/cleanVoteData.js
@@ -1,27 +1,12 @@
-const fs = require("fs").promises;
-const { cleanForMatch } = require('./cleanForMatch');
-
-async function cleanVoteData(json, songFile) {
+async function cleanVoteData(json) {
     const warnings = [];
     const data = json;
-
-    const content = await fs.readFile(songFile, "utf-8");
-    const songJson = JSON.parse(content);
-
-    const validSongNames = new Set(
-        songJson.songs.map(entry => cleanForMatch(entry.song.name))
-    );
 
     data.users.forEach(user => {
         const seen = new Map();
         const uniqueVotes = [];
 
         for (const vote of user.votes) {
-            if (!validSongNames.has(vote.song)) {
-                warnings.push(`${user.username} (${user.id}) had a vote for unknown song: ${vote.song}`);
-                continue;
-            }
-
             if (!seen.has(vote.song)) {
                 seen.set(vote.song, vote.vote);
                 uniqueVotes.push(vote);
