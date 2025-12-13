1060200e802ea5c872ad9b37ed3eac1b93d399cf
tdarth <97992436+tdarth@users.noreply.github.com>
2025-12-12 19:01:46 -0700

    revert

--- neru-bot/events/messageCreate.js
+++ neru-bot/events/messageCreate.js
@@ -16,8 +16,8 @@ module.exports = {
     if (message.guild) {
       for (const trigger of message.client.triggers) {
         try {
-          if (trigger.trigger(message.toLowerCase())) {
-            await trigger.execute(message.toLowerCase());
+          if (trigger.trigger(message)) {
+            await trigger.execute(message);
             break;
           }
         } catch (err) {
