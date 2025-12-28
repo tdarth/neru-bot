711e39354558cb8e41991ec25ca4b56fe5b02008
tdarth <97992436+tdarth@users.noreply.github.com>
2025-12-28 03:12:44 -0700

    remove unused bots

--- startup.js
+++ startup.js
@@ -15,27 +15,20 @@ run("npm", ["install"], "./neru-bot")
           .on("close", () => {
             run("npm", ["install"], "./miku-bot")
               .on("close", () => {
-                run("npm", ["install"], "./tagroles")
+                run("npm", ["install"], "./modmail")
                   .on("close", () => {
-                    run("npm", ["install"], "./modmail")
+                    run("npm", ["install"], "./suzuka")
                       .on("close", () => {
-                        run("npm", ["install"], "./suzuka")
+                        run("npm", ["install"], "./vocavote")
                           .on("close", () => {
-                            run("npm", ["install"], "./vocavote")
-                              .on("close", () => {
-                                run("npm", ["install"], "./aniwrapped")
-                                  .on("close", () => {
-                                    run("node", ["index.js"], "./neru-bot");
-                                    run("node", ["index.js"], "./teto-bot");
-                                    run("node", ["index.js"], "./nowplaying");
-                                    run("node", ["index.js"], "./miku-bot");
-                                    run("node", ["index.js"], "./tagroles");
-                                    run("node", ["index.js"], "./modmail");
-                                    run("node", ["index.js"], "./suzuka");
-                                    run("node", ["index.js"], "./vocavote");
-                                    run("node", ["index.js"], "./aniwrapped");
-                                  });
-                              });
+                            run("node", ["index.js"], "./neru-bot");
+                            run("node", ["index.js"], "./teto-bot");
+                            run("node", ["index.js"], "./nowplaying");
+                            run("node", ["index.js"], "./miku-bot");
+                            run("node", ["index.js"], "./tagroles");
+                            run("node", ["index.js"], "./modmail");
+                            run("node", ["index.js"], "./suzuka");
+                            run("node", ["index.js"], "./vocavote");
                           });
                       });
                   });
