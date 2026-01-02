7e8b678c5c299b19c5d018e3ae3ec2d4ecb7d5b2
tdarth <97992436+tdarth@users.noreply.github.com>
2026-01-01 17:52:40 -0700

    stop duplicate typing indicators

--- modmail/events/onType.js
+++ modmail/events/onType.js
@@ -1,6 +1,6 @@
 const { Events, EmbedBuilder } = require('discord.js');
 const { messages } = require('../messages.json');
-const { modmailShowUserTypingIndicators, modmailUseBuiltInTypingIndicators } = require('../config.json');
+const { modmailShowUserTypingIndicators, modmailUseBuiltInTypingIndicators, modmailUserTypingIndicatorsFetchCount } = require('../config.json');
 const { getChannelByUser } = require('../db/utils/helper');
 const { getChannelFromId } = require('../utils/getChannelFromId');
 
@@ -16,7 +16,7 @@ module.exports = {
 
         const modmailChannel = await getChannelFromId(typing.client, modmailChannelId) || null;
         if (!modmailChannel) return console.log(`[MODMAIL] User typing indicator error (channel not found): ${e}`)
-        
+
         const embed = new EmbedBuilder();
         embed.setAuthor({ name: typing?.user?.username || 'unknown', iconURL: `https://cdn.discordapp.com/avatars/${typing?.user?.id}/${typing?.user?.avatar}.png` });
         embed.setDescription(messages.info.USER_START_TYPING || '**Started typing...**');
@@ -26,6 +26,22 @@ module.exports = {
         try {
             if (modmailUseBuiltInTypingIndicators) return await modmailChannel.sendTyping();
 
+            let found = false;
+            const topModmailMessages = await modmailChannel.messages.fetch({ limit: modmailUserTypingIndicatorsFetchCount || 10 });
+
+            for (const message of topModmailMessages.values()) {
+                if (message.embeds) {
+                    for (const mmEmbed of message.embeds) {
+                        if (mmEmbed.description == (messages?.info?.USER_START_TYPING || '**Started typing...**')) {
+                            found = true
+                            break;
+                        }
+                    }
+                }
+                if (found) break;
+            }
+
+            if (found) return;
             await modmailChannel.send({
                 embeds: [embed]
             });
