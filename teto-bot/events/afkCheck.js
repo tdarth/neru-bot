e1a9c179f96a049723228f91b1878cd8707f60fa
tdarth <97992436+tdarth@users.noreply.github.com>
2026-07-16 16:24:17 -0700

    fix teto afk reaction

--- teto-bot/events/afkCheck.js
+++ teto-bot/events/afkCheck.js
@@ -14,7 +14,7 @@ module.exports = {
         const id = message.author.id;
 
         if (id in afkUsers) {
-            await message.react('<:teto_wave:1373197110815621201>');
+            await message.react('<:teto_wave:1369235578427670570>');
 
             if (message.member && message.member.manageable) {
                 await message.member.setNickname(afkUsers[id].username, `User returned.`).catch(error => {
