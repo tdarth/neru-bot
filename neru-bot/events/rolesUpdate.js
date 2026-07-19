25e7671b8d5fa73af86d6359c91f76dd8e6d57a6
tdarth <97992436+tdarth@users.noreply.github.com>
2026-07-19 15:12:49 -0700

    fix booster role check

--- neru-bot/events/rolesUpdate.js
+++ neru-bot/events/rolesUpdate.js
@@ -5,14 +5,14 @@ const { customRoles } = require('../utils/crHelper');
 module.exports = {
     name: Events.GuildMemberUpdate,
     async execute(oldMember, newMember) {
-        if (message.author.bot) return;
-        if (oldMember.roles.cache.size !== newMember.roles.cache.size) return;
+        if (newMember.user.bot) return;
+        if (oldMember.roles.cache.size === newMember.roles.cache.size) return;
 
         const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
 
         if (removedRoles.includes(levelRoles[100]) && customRoles[newMember.id].has_role) {
             try {
-                await newMember.guild.roles.remove(customRoles[newMember.id].role_id);
+                await newMember.guild.roles.delete(customRoles[newMember.id].role_id);
             } catch (e) {
                 const channel = await newMember.guild.channels.fetch(customRoleRequestChannelId);
                 await channel.send(`:warning: <@990500436047982602> **Failed to delete Custom Role after user stopped boosting**: <@${newMember.id}> <@&${customRoles[newMember.id].role_id}>`);
--- neru-bot/index.js
+++ neru-bot/index.js
@@ -15,6 +15,7 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildVoiceStates
   ],
