de96c8f775a2a7631a1aa5810255738a1aec381e
tdarth <97992436+tdarth@users.noreply.github.com>
2026-07-19 17:18:51 -0700

    fix

--- neru-bot/events/commands/acrtu.js
+++ neru-bot/events/commands/acrtu.js
@@ -11,9 +11,9 @@ module.exports = {
             if (message.author.id !== "990500436047982602") return await replyWithText(message, ':x: Only `tdarth-chan` can use this command.');
             const args = message.content.replace(`${prefix}sendtochannel `, '').split(' ');
 
-            if (args.length <= 2) return replyWithText(message, `:x: **Usage: ${prefix}acrtu roleid user**`);
+            if (args.length <= 1) return replyWithText(message, `:x: **Usage: ${prefix}acrtu roleid user**`);
 
-            setCustomRoleEntry(args[0], { has_role: true, role_id: String(args[1]) });
+            setCustomRoleEntry(args[1], { has_role: true, role_id: String(args[2]) });
             await replyWithText(message, "Done");
         } catch (e) {
             console.log(`Acrtu command error: ${e}`);
