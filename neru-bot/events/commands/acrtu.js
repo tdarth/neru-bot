5df35514f99227287857ba138edba4fdc02e870a
tdarth <97992436+tdarth@users.noreply.github.com>
2026-07-19 17:21:04 -0700

    add remove

--- neru-bot/events/commands/acrtu.js
+++ neru-bot/events/commands/acrtu.js
@@ -1,7 +1,7 @@
 require('discord.js');
 const { prefix } = require('../../config.json');
 const replyWithText = require("../../utils/replyWithText");
-const { setCustomRoleEntry } = require('../../utils/crHelper');
+const { setCustomRoleEntry, deleteCustomRoleEntry } = require('../../utils/crHelper');
 
 module.exports = {
     name: 'acrtu',
@@ -11,9 +11,14 @@ module.exports = {
             if (message.author.id !== "990500436047982602") return await replyWithText(message, ':x: Only `tdarth-chan` can use this command.');
             const args = message.content.replace(`${prefix}sendtochannel `, '').split(' ');
 
-            if (args.length <= 1) return replyWithText(message, `:x: **Usage: ${prefix}acrtu roleid user**`);
+            if (args.length <= 1) return replyWithText(message, `:x: **Usage: ${prefix}acrtu roleid user remove/add**`);
 
-            setCustomRoleEntry(args[1], { has_role: true, role_id: String(args[2]) });
+            if (args[3] == 'remove') {
+                deleteCustomRoleEntry(args[2]);
+            } else {
+                setCustomRoleEntry(args[2], { has_role: true, role_id: String(args[1]) });
+            }
+
             await replyWithText(message, "Done");
         } catch (e) {
             console.log(`Acrtu command error: ${e}`);
